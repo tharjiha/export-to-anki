0674ca8a8d76ea6bb64ddf7ac331cbfeba74cf1f
tharjiha <tharjiha@gmail.com>
2025-07-09 14:54:53 -0400

    adding comments

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ def write_file(file_path, content):
     with open(file_path, "w") as file:
         file.write(content)
 
+#class of card info
 class card:
     def __init__(self, front, back):
         self.front = front
         self.back = back
 
+#given the text convert them into card types
 def extractInfo(content):
     cards = content.split(';;')
     cards = cards[:-1]
@@ -27,6 +29,7 @@ def extractInfo(content):
         my_cards.append(thiscard)
     return my_cards
 
+#a way to seperate the bullets with <br> instead of \n
 def addBullet(text):
     content = ''
     if len(text.split('\n')) < 2:
@@ -68,19 +71,21 @@ def clozeCardFlipped(my_cards):
             content += '}} \n'
     return content
 
+#runs basic app
 @app.route('/')
 def home():
     return render_template('index.html', show_popup=False, download_popup=False)
 
+#runs for when textbox is submitted
 @app.route('/submit', methods=['POST'])
 def submit():
-    user_input = request.form['user_text']
-    if not user_input:
+    user_input = request.form['user_text'] #getting text from textbox
+    if not user_input: #if text is empty just return page back with error pop up box
         return render_template('index.html', show_popup=True, download_popup=False)
     
-    selected_card_type = request.form.get('cardType')
+    selected_card_type = request.form.get('cardType') #get cardType selected
 
-    my_cards = extractInfo(user_input)
+    my_cards = extractInfo(user_input) #convert it to card type
     if isinstance(my_cards, str):
         return render_template('index.html', show_popup=True, download_popup=False)
 
@@ -92,10 +97,11 @@ def submit():
     else:
         user_input = basicCard(my_cards)
 
-    write_file("cards.txt",user_input)
+    write_file("cards.txt",user_input) #write the cards in anki format to a .txt file
 
-    return render_template('index.html', show_popup=False, download_popup=True)
+    return render_template('index.html', show_popup=False, download_popup=True) #return back home with download pop up box showing
 
+#functions to download a file
 @app.route('/download')
 def download_file():
     return send_file('cards.txt', as_attachment=True)   
